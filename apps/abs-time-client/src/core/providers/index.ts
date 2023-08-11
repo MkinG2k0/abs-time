967674a30c5a00bccb610174e6375032acbd50e6
kamai <kamai122000@mail.ru>
2023-08-11 14:49:00 +0300

    feat: add vscode

--- apps/abs-time-client/src/core/providers/index.ts
+++ apps/abs-time-client/src/core/providers/index.ts
@@ -1,9 +1,10 @@
 import compose from 'compose-function'
-import { WithAntd } from 'core/providers/with-antd'
-import { WithLayout } from 'core/providers/with-layout'
-import { WithTheme } from 'core/providers/with-theme'
 
 import { WithStore } from './with-store'
 import { WithPersist } from './with-persist'
+
+import { WithAntd } from 'core/providers/with-antd'
+import { WithLayout } from 'core/providers/with-layout'
+import { WithTheme } from 'core/providers/with-theme'
 
 export const withProviders = compose(WithStore, WithPersist, WithAntd, WithTheme, WithLayout)
